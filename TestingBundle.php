33cd50e7c92955f0e285e0a95dc2cb316be400b5
Kevin Archer <ka@kevinarcher.ca>
2013-12-20 22:43:39 -0500

    Initial Bundle commit

---
+++ TestingBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ka\Bundle\TestingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class TestingBundle
+ *
+ * @author Kevin Archer <ka@kevinarcher.ca>
+ */
+class TestingBundle extends Bundle
+{
+}
